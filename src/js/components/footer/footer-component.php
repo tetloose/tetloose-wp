724c2179591908a9ad4bbd3a031ba6a23cb905c7
James Tetley <tetloose@gmail.com>
2023-05-10 13:34:57 +0100

    Moved footer fields to cloned from group

--- src/js/components/footer/footer-component.php
+++ src/js/components/footer/footer-component.php
@@ -5,34 +5,39 @@
  * @package Tetloose-Theme
  */
 
-$footer = get_field( 'footer', 'option' );
-$footer_styles = get_field( 'footer_styles', 'option' );
+$footer_navigation = get_field( 'footer_navigation', 'option' );
+$footer_description = get_field( 'footer_description', 'option' ) ? get_field( 'footer_description', 'option' ) : '';
+$footer_bg_borders = get_field( 'footer_bg_borders', 'option' );
+$footer_content_styles = get_field( 'footer_content_styles', 'option' );
+$footer_selection = get_field( 'footer_selection', 'option' );
 $footer_component = new Module(
     [
         'footer',
     ],
     [
         'u-animate-hide',
-        $footer_styles['bg_borders']['background_color'],
-        $footer_styles['bg_borders']['border_color']
-            ? 'u-border-t ' . $footer_styles['bg_borders']['border_color']
+        $footer_bg_borders['background_color'],
+        $footer_bg_borders['border_color']
+            ? 'u-border-t ' . $footer_bg_borders['border_color']
             : '',
-        $footer_styles['content_styles']['color'],
-        $footer_styles['content_styles']['link_color'],
-        $footer_styles['content_styles']['link_hover_color'],
-        $footer_styles['content_styles']['link_background_hover_color'],
-        $footer_styles['selection']['color'],
-        $footer_styles['selection']['background_color'],
+        $footer_content_styles['color'],
+        $footer_content_styles['link_color'],
+        $footer_content_styles['link_hover_color'],
+        $footer_content_styles['link_background_hover_color'],
+        $footer_selection['color'],
+        $footer_selection['background_color'],
     ]
 );
 $sub_nav_component = new Module(
     [
-        'u-animate-hide',
         'sub-nav',
         'is-inline',
-        $footer_styles['content_styles']['link_hover_color'],
-        $footer_styles['content_styles']['link_background_hover_color'],
+        $footer_content_styles['link_hover_color'],
+        $footer_content_styles['link_background_hover_color'],
     ],
+    [
+        'u-animate-hide',
+    ]
 );
 ?>
 <footer
@@ -52,30 +57,28 @@ $sub_nav_component = new Module(
     ?>
     <nav data-styles="footer__nav">
         <?php
-        if ( ! empty( $footer['navigation']->ID ) ) :
+        if ( ! empty( $footer_navigation->ID ) ) :
             get_template_part(
                 'components/navigation-component',
                 null,
                 array(
-                    'id' => $footer['navigation']->ID,
+                    'id' => $footer_navigation->ID,
                     'styles' => $sub_nav_component->styles(),
-                    'class_names' => '',
+                    'class_names' => $sub_nav_component->class_names(),
                 )
             );
         endif
         ?>
     </nav>
     <?php
-    if ( ! empty( $footer['copyright'] ) ) :
-        get_template_part(
-            'components/partials-content',
-            null,
-            array(
-                'styles' => 'footer__content',
-                'class_names' => '',
-                'content' => '<p><small>' . wp_kses_post( '<sup>&copy;</sup> ' . get_bloginfo() . ' ' . esc_attr( gmdate( 'Y' ) ) . ' ' . $footer['copyright'] ) . '</small></p>',
-            )
-        );
-    endif;
+    get_template_part(
+        'components/partials-content',
+        null,
+        array(
+            'styles' => 'footer__content',
+            'class_names' => '',
+            'content' => '<p><small>' . wp_kses_post( '<sup>&copy;</sup>' . esc_attr( gmdate( 'Y' ) ) . ' ' . get_bloginfo() . ' ' . $footer_description ) . '</small></p>',
+        )
+    );
     ?>
 </footer>
