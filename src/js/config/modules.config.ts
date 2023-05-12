0c9d63af6ac1b75091910558176de302e5e62196
James Tetley <tetloose@gmail.com>
2023-05-12 15:59:47 +0100

    moved excerpt into partial

--- src/js/config/modules.config.ts
+++ src/js/config/modules.config.ts
@@ -3,6 +3,5 @@ export const modules = {
     Navigation: () => import(/* webpackChunkName: "navigation" */ '../components/navigation/navigation.component'),
     Footer: () => import(/* webpackChunkName: "footer" */ '../components/footer/footer.component'),
     AddPosts: () => import(/* webpackChunkName: "add-posts" */ '../components/add-posts/add-posts.component'),
-    Excerpt: () => import(/* webpackChunkName: "excerpt" */ '../components/excerpt/excerpt.component'),
     PostNav: () => import(/* webpackChunkName: "post-nav" */ '../components/post-nav/post-nav.component')
 }
