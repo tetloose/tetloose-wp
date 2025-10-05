7df640c07187c4e7bfdce741b7dbf3ab671ba1f3
tetloose <tetloose@gmail.com>
2025-10-05 13:51:43 +0100

    chore: update header alignment

--- src/components/wrapper/wrapper-header.php
+++ src/components/wrapper/wrapper-header.php
@@ -17,14 +17,15 @@ $html_component = new Module(
 <!DOCTYPE html>
 <html
     <?php language_attributes(); ?>
-    class="<?php echo esc_attr( $html_component->class_names() ); ?>">
-<head>
-    <?php
-    get_template_part( '/components/wrapper', 'header-meta' );
-    get_template_part( '/components/wrapper', 'header-scripts' );
-    ?>
-</head>
-<body>
-    <?php
-    get_template_part( '/components/wrapper', 'body-scripts' );
-    get_template_part( '/components/header', 'component' );
+    class="<?php echo esc_attr( $html_component->class_names() ); ?>"
+>
+    <head>
+        <?php
+        get_template_part( '/components/wrapper', 'header-meta' );
+        get_template_part( '/components/wrapper', 'header-scripts' );
+        ?>
+    </head>
+    <body>
+        <?php
+        get_template_part( '/components/wrapper', 'body-scripts' );
+        get_template_part( '/components/header', 'component' );
